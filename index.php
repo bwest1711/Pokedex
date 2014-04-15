3f6a776da735eb828c942215a7e05b29a477ff46
Brandon West <bwest1711@gmail.com>
2014-04-14 20:23:18 -0400

    Started project

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <?php include('includes/head.php') ?>
+    </head>
+    <body>
+        <div id="container">
+            <div id="title">
+                <h1>Team Rocket</h1>
+            </div>
+            <form method="post" action="search.php">
+                <input type="text" title="search_bar" value="Search Trainers">
+                <input type="button" title="button" value="Go!"><br>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ search.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+    <?php include('includes/head.php')?>
+</head>
+<body>
+</body>
+</html>
