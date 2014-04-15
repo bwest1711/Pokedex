31ff4f299afe5fd367a725bf2d306307bb3d3cc8
Brandon West <bwest1711@gmail.com>
2014-04-15 18:56:45 -0400

    Testing the connection
    This file is just used to test the php mysql connection and queries.

--- connection.php
+++ connection.php
@@ -1,15 +1,3 @@
-<?php
-    echo "hello";
-    // Create connection
-    $con=mysqli_connect("localhost:8889","root","root","pokemon");
-
-    // Check connection
-    if (!$con) {
-      die( "Failed to connect to MySQL: " . mysqli_connect_error());
-    }
-    else {
-        echo "connected"
-    }
-
-    echo $query="SELECT * FROM trainers;";
-?> 
+<?php 
+    $connect = mysql_connect('localhost', 'root', 'root', 'pokedex') or die('unable to connect');
+?>
