e5cdce2c8a86b932918113ece319f192598facf4
Brandon West <bwest1711@gmail.com>
2014-04-16 16:05:41 -0400

    Comments

--- search.php
+++ search.php
@@ -1,26 +1,29 @@
 <?php
+    // Allows us to use $_SESSION[] variable
     session_start();
     
     // Connect to the database.
-    // Not sure if this works with a different db
     require 'connection.inc.php';
 
-    // There has to be a better way to do the variables.
+    // The name comes from the search box.
     $name = $_POST['name'];
 
     // Session variables accessible across all web pages
     $_SESSION['name'] = $name;
-    $_SESSION['pokemon'] = array('poke0','poke1', 'poke2', 'poke3', 'poke4', 'poke5');
+
+    // These are temporary
+    $_SESSION['pokemon'] = array('Name','Name', 'Name', 'Name', 'Name', 'Name');
     $_SESSION['abilities'] = array('0','1','2','3','4','5','6','7');
     
     
-    // Build the query as a string using the variables above
+    // Build the query as a string using the name
     $query = sprintf("SELECT hometown, gender, phone_number, momsname FROM trainer WHERE name ='%s'", 
-                     mysql_real_escape_string($name)) or die('text');
+                     mysql_real_escape_string($name)) or die('search query');
 
     // Query using that string 
     $result = mysql_query($query) or die('query');
 
+    // Check to make sure that the user exists. If they don't, transfer them to the registration page.
     if(mysql_num_rows($result) == 0){
         header('Location: registration_form.php');
         die();
@@ -40,6 +43,6 @@
     $_SESSION['phone_number'] =$phone_number;
     $_SESSION['moms_name'] =$moms_name;
 
+    // Transfer the user to their profile page.
     header('Location: trainer.php');
-?> 
-Hello
+?>
