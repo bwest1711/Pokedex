960daee8f2440dab434c96d997788a72adc98269
Brandon West <bwest1711@gmail.com>
2014-04-17 00:33:32 -0400

    Working on a new search method.
    Moving to a more object oriented version of mysqli

---
+++ new_search.php
@@ -0,0 +1,20 @@
+<?php
+
+$mysqli = new mysqli('localhost', 'root', 'root', 'pokedex');
+
+// Check connection
+if(mysqli_connect_errno()) {
+    printf('Connect failed: %s\n', mysqli_connect_errno());
+    exit();
+}
+
+// Reutrn the name of the current default database
+if($result = $mysqli->query("SELECT DATABASE()")) {
+    $row = $result-> fetch_row();
+    printf("Default database is %s.\n", $row[0]);
+    $result->close();
+}
+
+$mysqli->close();
+
+?>
